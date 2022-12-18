4d43c5e0a0b417056542b59eb364899660872d28
anveloper <hitedin@gmail.com>
2022-12-18 16:54:48 +0900

    feat: 구글 로그인 환경세팅

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
 
 const Login = () => {
-  return <div>Login</div>;
+  const responseGoogle = (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
+        buttonText="구글로 시작하기"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+    </div>
+  );
 };
 
 export default Login;
