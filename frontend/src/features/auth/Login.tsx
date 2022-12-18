9182180c42b319a1979d855a8e0bca1182dfbf03
anveloper <hitedin@gmail.com>
2022-12-18 17:31:56 +0900

    feat: 구글 로그인 환경 수정 중

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,25 +1,44 @@
+import axios from "axios";
 import React from "react";
 import {
+  CredentialResponse,
   GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
+  GoogleOAuthProvider,
+} from "@react-oauth/google";
 
 const Login = () => {
-  const responseGoogle = (
-    response: GoogleLoginResponse | GoogleLoginResponseOffline
-  ) => {
-    console.log(response);
+  const responseGoogle = (e: Error) => {
+    console.log(e);
   };
+  const onGoogleSignInSuccess = (res: CredentialResponse) => {
+    console.log(res);
+    // const params = new URLSearchParams();
+    // params.append("idToken", res.clientId.);
 
+    // const googleLogin = async () => {
+    //   const res = await axios.post("요청 주소", params, {
+    //     headers: {
+    //       "Content-Type": "application/x-www-form-urlencoded",
+    //     },
+    //   });
+    //   console.log(res);
+    //   localStorage.setItem("accessToken", res.data.token.access);
+    //   localStorage.setItem("refreshToken", res.data.token.refresh);
+    // };
+
+    // googleLogin();
+  };
   return (
     <div>
-      <GoogleLogin
+      <GoogleOAuthProvider
         clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
-        buttonText="구글로 시작하기"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />
+      >
+        <GoogleLogin
+          text="signup_with"
+          onSuccess={onGoogleSignInSuccess}
+          onError={() => alert("실패")}
+        />
+      </GoogleOAuthProvider>
     </div>
   );
 };
