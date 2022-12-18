6be283b0312768d4ebcd5244ff864ceb981634ba
최진호 <84559471+turtlebooster@users.noreply.github.com>
2022-12-18 15:46:36 +0900

    feat: BE 초기 세팅

---
+++ backend/selectool/src/main/java/com/threejeans/selectool/SelectoolApplication.java
@@ -0,0 +1,13 @@
+package com.threejeans.selectool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectoolApplication.class, args);
+    }
+
+}
---
+++ backend/selectool/src/test/java/com/threejeans/selectool/SelectoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threejeans.selectool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
