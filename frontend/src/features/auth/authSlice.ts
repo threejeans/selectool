c203aeeb29259bd9b2cb490a6f1585ae275bf1eb
anveloper <hitedin@gmail.com>
2022-12-26 18:04:28 +0900

    feat: 로그인 모달 전역 접근을 위한 상태값 추가

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "app/store";
+
+export interface AuthState {
+  isLoginModal: boolean;
+}
+
+const initialState: AuthState = {
+  isLoginModal: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginModalOpen: (state) => {
+      console.log(state);
+      state.isLoginModal = true;
+    },
+    loginModalClose: (state) => {
+      console.log(state);
+      state.isLoginModal = false;
+    },
+  },
+});
+
+export const { loginModalOpen, loginModalClose } = authSlice.actions;
+
+export const selectLoginModal = (state: RootState) => state.auth.isLoginModal;
+
+export default authSlice.reducer;
