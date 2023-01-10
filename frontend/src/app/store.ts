600fd3d26ed9ca34d9e7e7506566e06bc5c4300b
anveloper <hitedin@gmail.com>
2023-01-10 17:50:00 +0900

    feat: axios 미들웨어 및 인터셉터 추가

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 import authReducer from 'features/auth/authSlice'
+import { axiosMiddleware } from './apiAxios'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+  },
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware().concat(axiosMiddleware)
   },
 })
 
