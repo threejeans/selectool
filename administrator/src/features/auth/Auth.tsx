9e4946ac9f2f976a24ee204e3d1847928ee6d152
anveloper <hitedin@gmail.com>
2023-01-22 23:31:48 +0900

    feat: 인증 페이지 컴포넌트 구현중

--- administrator/src/features/auth/Auth.tsx
+++ administrator/src/features/auth/Auth.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectTmpEmail } from "./authSlice";
+
 import styles from "styles/pages/auth/Auth.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
+  const tmpEmail = useSelector(selectTmpEmail);
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(tmpEmail);
+    if (!tmpEmail) {
+      navigate("/admin");
+    }
+  }, []);
+
   return <div className={styles.container}></div>;
 };
 
