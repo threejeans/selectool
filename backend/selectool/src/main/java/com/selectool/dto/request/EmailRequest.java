fc410b8ba50b7904b66dfe2975ca826ce499ee25
최진호 <84559471+turtlebooster@users.noreply.github.com>
2023-01-25 17:03:56 +0900

    feat: 이메일 전송 요청 DTO 생성

---
+++ backend/selectool/src/main/java/com/selectool/dto/request/EmailRequest.java
@@ -0,0 +1,20 @@
+package com.selectool.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmailRequest {
+    private String address;
+    private String title;
+    private String content;
+
+    @Builder
+    public EmailRequest(String address, String title, String content) {
+        this.address = address;
+        this.title = title;
+        this.content = content;
+    }
+}
