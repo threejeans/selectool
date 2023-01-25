a15f7a7ee6add10e783a74170cb9d9e516947ea5
최진호 <84559471+turtlebooster@users.noreply.github.com>
2023-01-25 17:04:50 +0900

    feat: 이메일 생성, 전송 서비스 구현

--- backend/selectool/src/main/java/com/selectool/exception/NotFoundException.java
+++ backend/selectool/src/main/java/com/selectool/exception/NotFoundException.java
@@ -4,6 +4,7 @@ public class NotFoundException extends RuntimeException {
 //    public static final String FESTIVAL_NOT_FOUND = "존재하지 않는 축제입니다.";
     public static final String USER_NOT_FOUND = "존재하지 않는 유저입니다.";
     public static final String AUTH_NOT_FOUND = "존재하지 않는 REFRESH TOKEN 입니다.";
+    public static final String ADMIN_NOT_FOUND = "존재하지 않는 관리자입니다.";
 
     public NotFoundException(String message) {
         super(message);
--- backend/selectool/src/main/java/com/selectool/service/MailService.java
+++
@@ -1,8 +0,0 @@
-package com.selectool.service;
-
-import com.selectool.dto.request.MailRequest;
-
-public interface MailService {
-    MailRequest createMail(String userEmail);
-    void sendMail(MailRequest mailRequest);
-}
