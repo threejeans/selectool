c405845e49573bef16891aec2f38c0353e816cec
최진호 <84559471+turtlebooster@users.noreply.github.com>
2023-01-26 18:00:27 +0900

    feat: 관리자 로그인 api 구현

--- backend/selectool/src/main/java/com/selectool/controller/AdminController.java
+++ backend/selectool/src/main/java/com/selectool/controller/AdminController.java
@@ -3,16 +3,22 @@ package com.selectool.controller;
 import com.selectool.config.loginuser.LoginUser;
 import com.selectool.config.loginuser.User;
 import com.selectool.dto.request.CodeRequest;
+import com.selectool.dto.response.ServiceTokenResponse;
 import com.selectool.entity.Code;
 import com.selectool.service.AdminService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 
 @RestController
@@ -21,6 +27,9 @@ import java.io.UnsupportedEncodingException;
 @Api(tags = "관리자")
 public class AdminController {
     private final AdminService adminService;
+
+    @Value("${token.refresh_token.expiration_time}")
+    private Long REFRESH_EXPIRATION;
 
     @PostMapping("/login")
     @ApiOperation(value = "관리자 이메일로 로그인 코드 전송")
@@ -31,8 +40,19 @@ public class AdminController {
 
     @PostMapping("/auth")
     @ApiOperation(value = "관리자 코드로 로그인")
-    public ResponseEntity<?> loginAdmin(@RequestBody CodeRequest request) {
-        adminService.login(request);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> loginAdmin(@RequestBody CodeRequest request, HttpServletResponse response) {
+        ServiceTokenResponse tokenResponse = adminService.login(request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", tokenResponse.getAccessToken());
+        Cookie cookie = new Cookie("refresh-token", tokenResponse.getRefreshToken());
+        cookie.setMaxAge(REFRESH_EXPIRATION.intValue());
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setDomain("");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(headers)
+                .build();
     }
 }
