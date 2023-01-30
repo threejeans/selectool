d7cb48c3ea4e2e91e68693cd526931158d0994eb
anveloper <hitedin@gmail.com>
2023-01-30 23:39:21 +0900

    refactor: 기존 관리자 페이지 프로젝트 삭제

--- administrator/src/features/contents/Self.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Self = () => {
-  return <div>Self</div>;
-};
-
-export default Self;
--- administrator/src/features/contents/With.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const With = () => {
-  return <div>With</div>;
-};
-
-export default With;
