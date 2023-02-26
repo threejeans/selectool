0d169403b39997602624f2143a06d62e62645d1b
최진호 <84559471+turtlebooster@users.noreply.github.com>
2023-02-26 19:10:54 +0900

    feat(swagger): ToolResponse 모델 설명 추가

--- backend/selectool/src/main/java/com/selectool/dto/tool/response/ToolResponse.java
+++ backend/selectool/src/main/java/com/selectool/dto/tool/response/ToolResponse.java
@@ -1,6 +1,8 @@
 package com.selectool.dto.tool.response;
 
 import com.selectool.entity.ToolCategory;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,37 +11,53 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@ApiModel(value = "툴 응답 모델")
 public class ToolResponse {
     private Long id;
 
+    @ApiModelProperty(value = "툴 국문명")
     private String nameKr;
 
+    @ApiModelProperty(value = "툴 영문명")
     private String nameEn;
 
+    @ApiModelProperty(value = "툴 한 줄 소개")
     private String info;
 
+    @ApiModelProperty(value = "툴 호버 메세지")
     private String msg;
 
+    @ApiModelProperty(value = "툴 토픽")
     private String topic;
 
+    @ApiModelProperty(value = "툴 국가")
     private String country;
 
+    @ApiModelProperty(value = "툴 썸네일 이미지 주소")
     private String image;
 
+    @ApiModelProperty(value = "툴 사이트 주소")
     private String url;
 
+    @ApiModelProperty(value = "툴 안드로이드 평점")
     private String aos;
 
+    @ApiModelProperty(value = "툴 애플 평점")
     private String ios;
 
+    @ApiModelProperty(value = "툴 북마크 여부")
     private Boolean isBookmarked;
 
+    @ApiModelProperty(value = "툴 분류 목록")
     private List<ToolCategoryResponse> categories;
 
+    @ApiModelProperty(value = "툴 핵심 기능 목록")
     private List<ToolFunctionResponse> toolFunctions;
 
+    @ApiModelProperty(value = "주요 고객 목록")
     private List<ClientResponse> clients;
 
+    @ApiModelProperty(value = "툴 가격 플랜 목록")
     private List<ToolPlanResponse> plans;
 
     @Builder
