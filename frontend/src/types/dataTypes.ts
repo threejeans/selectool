bc44657b13e91cd654627750d1f377a084a0ba67
anveloper <hitedin@gmail.com>
2023-03-01 01:48:21 +0900

    feat: 혼자써요 tmp 타입 분리

--- frontend/src/types/dataTypes.ts
+++ frontend/src/types/dataTypes.ts
@@ -1,7 +1,8 @@
 // 공통 type 지정
 
-export {}
+import { DateYMD } from 'react-aws-s3-typescript/dist/types'
 
+// tool, 혼자써요 필요 type
 export type CategoryType = {
   name: string
 }
@@ -10,7 +11,7 @@ export type ToolFuncType = {
   content: string
 }
 export type ClientType = {
-  id: number
+  id: number | ''
   name: string
   image: string
   url: string
@@ -24,7 +25,27 @@ export type PlanType = {
   cost: string
   planFunctions: PlanFunctionType[]
 }
-
+export type TmpToolMainType = {
+  // main
+  nameKr: string
+  nameEn: string
+  info: string
+  msg: string
+  topic: string
+  categories: CategoryType[]
+  country: string
+  image: string
+}
+export type TmpToolSpecificType = {
+  // specific
+  url: string
+  toolFunctions: ToolFuncType[]
+  clients: ClientType[]
+  plans: PlanType[]
+  aos: string
+  ios: string
+}
+// create 시 필요한 모든 데이터
 export type ToolType = {
   // main
   nameKr: string
@@ -42,4 +63,19 @@ export type ToolType = {
   plans: PlanType[]
   aos: string
   ios: string
+}
+
+// 가이드
+
+export type GuideType = {
+  title: string
+  date?: Date
+  content: string
+  source: string
+  toolName: string
+  func: string
+  categories: CategoryType[]
+  url: string
+  image: string
+  toolImage: string
 }
