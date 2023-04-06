b927af1947f93da6906a541e124b16993dfbada2
anveloper <hitedin@gmail.com>
2023-04-06 22:07:24 +0900

    feat: 복수선택 안내 메세지 추가

--- frontend/src/containers/Guide/GuideControl.tsx
+++ frontend/src/containers/Guide/GuideControl.tsx
@@ -126,6 +126,9 @@ const GuideControl = () => {
                 </div>
               )
             })}
+            {categoryList.length > 0 && (
+              <span className={styles.duple}>* 복수 선택이 가능해요.</span>
+            )}
           </div>
         </div>
         <div className={styles.dropBox}>
