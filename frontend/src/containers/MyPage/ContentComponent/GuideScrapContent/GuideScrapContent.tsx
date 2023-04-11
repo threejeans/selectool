feac2ddfa9b00bbcdb0c60a0b54f9da880da31ca
Jin-Yuna <dbsdk100@gmail.com>
2023-04-11 11:13:06 +0900

    feat: 로그인 반응형 완료 / 마이페이지 가이드 스크랩 완료

--- frontend/src/containers/MyPage/ContentComponent/GuideScrapContent/GuideScrapContent.tsx
+++ frontend/src/containers/MyPage/ContentComponent/GuideScrapContent/GuideScrapContent.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CommonLayout } from '../../CommonComponent'
+import { useAppDispatch, useAppSelector } from 'app/hooks'
+import {
+  resetGuideScrapCount,
+  setGuideScrapList,
+} from 'reducers/settingReducer'
+import { getMemberGuideList, selectGuideList } from 'reducers/guideReducer'
+import GuideScrapCardGrid from 'containers/MyPage/CommonComponent/CardGrid/GuideScrapCardGrid'
 
 const GuideScrapContent = () => {
+  const mainInfoList = useAppSelector(selectGuideList)
+
+  const dispatch = useAppDispatch()
+
+  const getScrapList = async () => {
+    dispatch(resetGuideScrapCount())
+
+    if (mainInfoList.length == 0) {
+      await dispatch(getMemberGuideList())
+    }
+
+    const newScrapList = mainInfoList.filter(item => item.isBookmarked === true)
+    dispatch(setGuideScrapList(newScrapList))
+  }
+
+  useEffect(() => {
+    getScrapList()
+  }, [mainInfoList])
+
   return (
     <CommonLayout type='bookmark'>
-      <div>가이드</div>
+      <GuideScrapCardGrid />
     </CommonLayout>
   )
 }
--- frontend/src/features/notFound404/NotFound404.tsx
+++ frontend/src/features/notFound404/NotFound404.tsx
@@ -1,29 +1,76 @@
+import { MobileWide, Pc, Tablet } from 'components/Layout'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from 'styles/pages/notFound404/NotFound404.module.css'
 
 const NotFound404 = () => {
   return (
-    <div className={styles.layout}>
-      <div className={styles.container}>
-        <div>
-          <div className={styles.mainTitle}>404</div>
-          <div className={styles.textSection}>
-            <div className={styles.mainText}>
-              아쉽지만 요청하신 페이지를 찾을 수 없어요 :&#40;
+    <>
+      <Pc>
+        <div className={styles.layout}>
+          <div className={styles.container}>
+            <div>
+              <div className={styles.mainTitle}>404</div>
+              <div className={styles.textSection}>
+                <div className={styles.mainText}>
+                  아쉽지만 요청하신 페이지를 찾을 수 없어요 :&#40;
+                </div>
+                <div className={styles.subText}>
+                  입력하신 페이지가 변경, 삭제 되었거나 존재하지 않는 주소를
+                  입력하셨어요.
+                </div>
+              </div>
             </div>
-            <div className={styles.subText}>
-              입력하신 페이지가 변경, 삭제 되었거나 존재하지 않는 주소를
-              입력하셨어요.
-            </div>
+
+            <Link to='/' className={styles.linkText}>
+              홈으로
+            </Link>
           </div>
         </div>
+      </Pc>
+      <Tablet>
+        <div className={styles.layout}>
+          <div className={styles.container}>
+            <div>
+              <div className={styles.mainTitle}>404</div>
+              <div className={styles.textSection}>
+                <div className={styles.mainText}>
+                  아쉽지만 요청하신 페이지를 찾을 수 없어요 :&#40;
+                </div>
+                <div className={styles.subText}>
+                  입력하신 페이지가 변경, 삭제 되었거나 존재하지 않는 주소를
+                  입력하셨어요.
+                </div>
+              </div>
+            </div>
 
-        <Link to='/' className={styles.linkText}>
-          홈으로
-        </Link>
-      </div>
-    </div>
+            <Link to='/' className={styles.linkText}>
+              홈으로
+            </Link>
+          </div>
+        </div>
+      </Tablet>
+      <MobileWide>
+        <div className={styles.layout}>
+          <div className={styles.container_mobile}>
+            <div className={styles.mainTitle_mobile}>404</div>
+            <div className={styles.textSection_mobile}>
+              <div className={styles.mainText_mobile}>
+                아쉽지만 요청하신 페이지를 찾을 수 없어요 :&#40;
+              </div>
+              <div className={styles.subText_mobile}>
+                입력하신 페이지가 변경, 삭제 되었거나 존재하지 않는 주소를
+                입력하셨어요.
+              </div>
+            </div>
+
+            <Link to='/' className={styles.linkText_mobile}>
+              홈으로
+            </Link>
+          </div>
+        </div>
+      </MobileWide>
+    </>
   )
 }
 
